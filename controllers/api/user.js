72f45ec19864bf89b375e229252602ef9e5b7663
Steven Robertson <s2robertson@gmail.com>
2023-06-11 21:57:36 -0400

    added /models/index.js

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const User = require('../../models/User');
+const { User } = require('../../models/');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
-        console.log(`Attempting to create user: ${req.body}`);
         const user = await User.create(req.body);
         res.status(201).json(user);
     } catch (err) {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
 })
 

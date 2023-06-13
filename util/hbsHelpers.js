3a41cf89761508672a484b3f7f33d9c051feff89
Steven Robertson <s2robertson@gmail.com>
2023-06-13 09:43:34 -0400

    made comments list 1-indexed

--- util/hbsHelpers.js
+++ util/hbsHelpers.js
@@ -1,5 +1,10 @@
 module.exports = {
     format_date(d) {
         return d.toLocaleString();
+    },
+    inc(num, options) {
+        const base = parseInt(num);
+        const offset = options.hash.by ? parseInt(options.hash.by) : 1;
+        return base + offset;
     }
 }
